74bca4e76bb236ff0f32e4731b8b16b570e05eff
Phùng Đức Nhật <ducnhatphung@gmail.com>
2016-11-26 13:26:54 +0700

    config success

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,2 @@
 /// <reference path="globals/electron/index.d.ts" />
-/// <reference path="globals/hammerjs/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,6 @@ let webpack = require('webpack');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-let helpers = require('./config/helpers');
-
 
 module.exports = {
     devtool: 'source-map',
@@ -14,7 +12,22 @@ module.exports = {
     target: 'electron',
 
     entry: {
-        'render': './src/render/main.ts'
+        'render': './src/render/main.ts',
+        'vendor': [
+          "reflect-metadata",
+          "rxjs",
+          "zone.js",
+          "@angular/common",
+          "@angular/compiler",
+          "@angular/core",
+          "@angular/forms",
+          "@angular/http",
+          "@angular/material",
+          "@angular/platform-browser",
+          "@angular/platform-browser-dynamic",
+          "@angular/router",
+          "@angular/upgrade",
+        ]
     },
 
     output: {
@@ -26,24 +39,25 @@ module.exports = {
     },
 
     resolve: {
-        extensions: ['', '.ts', '.js', '.json', '.css', '.html'],
+        extensions: ['', '.ts', '.js', '.json', '.css', '.html']
     },
 
     module: {
       loaders: [{
         test: /\.ts$/,
-        loaders: ['ts']
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
       },
       {
         test: /\.html$/,
-        loader: 'html'
+        loader: 'raw'
       },
       {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file?name=assets/[name].[ext]'
+        loader: 'file?name=[name].[ext]'
       },
       {
         test: /\.css$/,
+        exclude: path.resolve('./src/render'),
         loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
       },
       {
